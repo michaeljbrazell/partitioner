5b44aede90793ff198779edc85aa0c081d1b5021
michaeljbrazell <michaeljbrazell@gmail.com>
2016-07-08 16:59:16 -0600

    Update makeall.sh

--- makeall.sh
+++ makeall.sh
@@ -12,11 +12,11 @@ mpif90 -O2  -DUSE_MPI -ffree-line-length-none  my_kinddefs.F90 connection_module
 ##rm *.mod
 ##mpif90 -O2  -DUSE_MPI  my_kinddefs.F90 connection_module.F90 mpi_schedule_module.F90 mesh_module.F90 line_module.F90  partition_module.F90  load_mesh.F90 setup_mesh.F90 partitioner.F90  -o partitioner.mpi -L/glade/u/home/mbrazell/Codes/metis-5.1.0/build/Linux-x86_64/libmetis -lmetis
 
-./partitioner.serial -p 16 gmsh/arcp2.msh
-./partitioner.serial -p 4 gmsh/arc.msh
-./partitioner.serial -p 5 gmsh/pripyrtet.msh
-./partitioner.serial -p 6 gmsh/transfinite.msh
+#./partitioner.serial -p 16 gmsh/arcp2.msh
+#./partitioner.serial -p 4 gmsh/arc.msh
+#./partitioner.serial -p 5 gmsh/pripyrtet.msh
+#./partitioner.serial -p 6 gmsh/transfinite.msh
 
-mpirun -np 4 ./partitioner.mpi -p 8 bump2d.mcell.unf
+#mpirun -np 4 ./partitioner.mpi -p 8 bump2d.mcell.unf
 
 
